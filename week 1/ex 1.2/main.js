7d4191f8fff29a6933787d0825ded131bdd6adbf
PrincessCoder <72946882+PrincessCoder@users.noreply.github.com>
2022-01-19 14:51:56 -0600

    ex 1.2
    Color update

--- week 1/ex 1.2/main.js
+++ week 1/ex 1.2/main.js
@@ -1,8 +1,8 @@
 const width = window.prompt("Please enter a number for the width of the ellipse.");
-const height = window.prompt("Please enter a number for the height of the ellipse. Draw by pressing on your mouse in the gray rectangle.");
+const height = window.prompt("Please enter a number for the height of the ellipse. Draw by pressing on your mouse.");
 function setup(){
-    createCanvas(480, 120)
-    background(204);
+    createCanvas(1920, 1080)
+    background(0,0,255);
 }
 function draw(){
     if (mouseIsPressed){
