f13c09d750a684bb9936112afe66c5ab7cfcbd4e
PrincessCoder <72946882+PrincessCoder@users.noreply.github.com>
2022-02-08 17:50:03 -0600

    Update 4.1 Exercise DOM

--- week4/main.js
+++ week4/main.js
@@ -2,7 +2,7 @@
 const myEmptyDiv = document.querySelector('#myEmptyDiv');
 // put an h1 inside of it
 // create a new element
-const heading = document.createElement('h1');
+const heading = document.createElement ('h1');
 heading.innerHTML = 'Hello Ken! It is a sunny day in Green Bay, WI.';
 heading.style.cursor = 'pointer';
 heading.addEventListener('click', handleHeadingClick);
