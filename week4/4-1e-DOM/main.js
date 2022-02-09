2c80e2ceb688e9e79b0dc18f095dabac8f5dbd57
PrincessCoder <72946882+PrincessCoder@users.noreply.github.com>
2022-02-08 18:01:09 -0600

    llama comment

--- week4/4-1e-DOM/main.js
+++ week4/4-1e-DOM/main.js
@@ -3,7 +3,7 @@ const myEmptyDiv = document.querySelector('#myEmptyDiv');
 // put an h1 inside of it
 // create a new element
 const heading = document.createElement ('h1');
-heading.innerHTML = 'Hello Ken! It is a sunny day in Green Bay, WI.';
+heading.innerHTML = 'Hello Ken! It is a sunny day in Green Bay, WI. The llamas are sunning here at the zoo.';
 heading.style.cursor = 'pointer';
 heading.addEventListener('click', handleHeadingClick);
 myEmptyDiv.appendChild(heading);
