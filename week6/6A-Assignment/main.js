d7879c0b278bb04702a2ed3abe7b66390974d998
PrincessCoder <72946882+PrincessCoder@users.noreply.github.com>
2022-02-22 19:19:18 -0600

    6A Assignment Progress

---
+++ week6/6A-Assignment/main.js
@@ -0,0 +1,16 @@
+new Vue({
+    el:"#myApp",
+    data: {
+        headline: "10 Fun Facts about Llamas",
+        llamaSmart: "Llamas are smart.",
+        llamaLife: "A llama's lifespan is 20 years.",
+        llamaSocial: "Llamas are very social.",
+        llamaTall: "Llamas can grow as much as 6 feet tall.",
+        llamaHum: "Llamas can hum.",
+        llamaTherapy: "Llamas are used as therapy animals.",
+        llamaYarn: "Yarn made from llama fiber is extremely versatile.",
+        llamaSpit: "Llamas can shoot green spit up to 10 feet away.",
+        llamaVeg: "Llamas are vegetarians.",
+        llamaCamelid: "Llamas are members of the Camelid family"
+    }
+})
