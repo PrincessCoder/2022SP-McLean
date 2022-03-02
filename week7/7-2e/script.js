893484224f4b748c5aebc2f73832d2028e31d2ab
PrincessCoder <72946882+PrincessCoder@users.noreply.github.com>
2022-03-02 16:50:10 -0600

    7.2 started

---
+++ week7/7-2e/script.js
@@ -0,0 +1,9 @@
+// Vue.createApp({
+//     data() {
+//         return {
+//             llamaLand: "https://en.wikipedia.org/wiki/Llama",
+//             llamaTitle: "Lama glama",
+//             alpacaLand: "https://en.wikipedia.org/wiki/Alpaca",
+//             alpacaTitle: "Lama pacos"
+//       }
+// }).mount("#myApp");
