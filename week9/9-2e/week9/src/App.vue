c44f0b8254aa9798df0098b8cb4dd5e6208f6ebb
PrincessCoder <72946882+PrincessCoder@users.noreply.github.com>
2022-03-24 21:55:46 -0500

    9.2e

---
+++ week9/9-2e/week9/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+//javascript here
+const isWorking = true;
+</script>
+
+<template>
+<!--html goes here-->
+<h1>My first compiled Vue app</h1>
+<div v-if="isWorking">It is working</div>
+</template>
+
+<style>
+/*style goes here */
+.container {
+  width: 600px;
+  margin: 20px auto 0;
+  text-align: center;
+  font-size: 18 px;
+}
+</style>
