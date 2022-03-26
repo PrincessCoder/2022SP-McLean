4bf35827d030cbcd04e3f24ec0382f892d79904f
PrincessCoder <72946882+PrincessCoder@users.noreply.github.com>
2022-03-26 15:14:09 -0500

    9-2e Update

--- week9/9-2e/cli-project/src/App.vue
+++ week9/9-2e/cli-project/src/App.vue
@@ -1,13 +1,39 @@
 <script setup>
 //javascript here
 const isWorking = true;
+ const animals = [
+      {
+        name: "Lama",
+        binomialName: "Lama glama",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Lama",
+        species: "L. glama",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg",
+        id: 0
+        },
+      {
+        name: "Alpaca",
+        binomialName: "Vicugna pacos",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Vicugna",
+        species: "V. pacos",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg",
+        id: 1
+      },
+    ]
 </script>
 
 <template>
 <!--html goes here-->
 <div class="container">
-  <h1>My first compiled Vue app</h1>
-  <div v-if="isWorking">It is working</div>
+  <h1>Camelids</h1>
+  <div v-for="(animal, index) in animals" v-bind:key="index" class="animal-row">
+    <h2>{{animal.name}}</h2>
+    <h5>{{animal.binomialName}}, {{animal.order}}, {{animal.family}}, {{animal.genus}}, {{animal.species}}</h5>
+    <img v-bind:src="animal.image" v-bind:alt="animal.name">
+  </div>
 </div>
 </template>
 
@@ -16,7 +42,14 @@ const isWorking = true;
 .container {
   width: 600px;
   margin: 20px auto 0;
-  text-align: center;
   font-size: 18 px;
 }
+.container h1 {
+  text-align: left;
+}
+.container .animal-row img {
+  width: 300px;
+  display:block;
+}
+
 </style>
